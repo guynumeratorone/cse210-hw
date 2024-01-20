3e96ad695478599d1d7dd4b76a846c2b7da7ac74
Joe Allen <Jelderallen@yahoo.com>
2024-01-20 15:29:42 -0700

    late assignment completed late

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //asks for first name
+        Console.WriteLine("What is your first name?");
+        string userFirstName = Console.ReadLine();
+
+        //asks for last name
+        Console.WriteLine("What is your last name?");
+        string userLastName = Console.ReadLine();
+
+
+        //displays both names
+        Console.WriteLine($"Your name is {userLastName}, {userFirstName} {userLastName}");
     }
 }
